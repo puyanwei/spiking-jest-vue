7f7915c40074b9d2ac3e870d60b4f900753dd0d1
Puyan Wei <puyanwei@gmail.com>
2018-03-11 09:22:36 +0000

    added MessageList.vue

---
+++ src/components/MessageList.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <ul>
+    <li v-for="messages in message">{{ message }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "list",
+  props: ["messages"]
+};
+</script>
+
+<style lang="css">
+</style>
